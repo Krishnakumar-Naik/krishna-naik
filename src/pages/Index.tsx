0cca3b7b700f085f441a3da36a84d92e94431c6f
Krishnakumar Naik <krishnaknaik25@gmail.com>
2025-07-14 15:34:00 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,7 +5,6 @@ import Hero from '../components/Hero';
 import About from '../components/About';
 import Projects from '../components/Projects';
 import Experience from '../components/Experience';
-import Education from '../components/Education';
 import Skills from '../components/Skills';
 import Contact from '../components/Contact';
 import Footer from '../components/Footer';
@@ -18,7 +17,6 @@ const Index = () => {
       <About />
       <Projects />
       <Experience />
-      <Education />
       <Skills />
       <Contact />
       <Footer />
