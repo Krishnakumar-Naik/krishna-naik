6151075ea754439e8377f28b301a1e748bffad3e
Krishnakumar Naik <krishnaknaik25@gmail.com>
2025-10-28 19:31:20 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -11,7 +11,7 @@ import Footer from '../components/Footer';
 
 const Index = () => {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-slate-900 via-purple-900 to-slate-900">
+  <div className="min-h-screen bg-gradient-to-br from-slate-900 via-[#3a0a40] to-slate-900">
       <Header />
       <Hero />
       <About />
